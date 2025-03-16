252047916ec7dc06cf879dfdef4243c180e60677
Christina Mekheil <mekheilc@gmail.com>
2025-03-16 18:55:24 -0400

    php

--- php/components/header.php
+++ php/components/header.php
@@ -6,6 +6,10 @@
         <li><a href="./introduction.php">Introduction </a></li> 
         <li><a href="./contract.php">Contract </a></li> 
         <li><a href="./web250/">Static Site </a></li> 
+        <li><a href="./superduper_static/">Multipage Static </a></li> 
+        <li><a href="./superduper_php/">Multipage PHP </a></li> 
+        <li><a href="./joy/">Joy of PHP </a></li> 
+        <li><a href="./joy/samsusedcars.html">Sam's Used Cars </a></li> 
     </ul>
     </nav>
 </header>
